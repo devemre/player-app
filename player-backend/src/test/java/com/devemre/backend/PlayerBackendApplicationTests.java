524d6357f9626168485ec2850b8ad9c9e35c708f
devemre <dev.emre.aydin@gmail.com>
2024-06-08 16:34:22 +0300

    Update PlayerBackendApplicationTests.java

--- player-backend/src/test/java/com/devemre/backend/PlayerBackendApplicationTests.java
+++ player-backend/src/test/java/com/devemre/backend/PlayerBackendApplicationTests.java
@@ -1,13 +1,8 @@
 package com.devemre.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PlayerBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
